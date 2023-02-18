a911a07092a28fc436c247d0637e78b06a857281
Omotayo Olabode <tayo.olabode@senior.co.uk>
2023-02-18 16:48:01 +0000

    Updated model

---
+++ MyHrProject/Migrations/20230218163818_Updated staff model.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyHrProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updatedstaffmodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Staff",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Emailaddress",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmergencyContactFullName",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmergencyContactPhoneNumber",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PersonalPhoneNumber",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "WorkPhoneNumber",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "Emailaddress",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "EmergencyContactFullName",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "EmergencyContactPhoneNumber",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "PersonalPhoneNumber",
+                table: "Staff");
+
+            migrationBuilder.DropColumn(
+                name: "WorkPhoneNumber",
+                table: "Staff");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Staff",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- MyHrProject/Models/Staff.cs
+++ MyHrProject/Models/Staff.cs
@@ -8,10 +8,24 @@ public class Staff
     [Key]
     public Guid Id { get; set; }
     [Display(Name = "First Name")]
-    public string? FirstName { get; set; } 
+    public string FirstName { get; set; } 
     [Display(Name = "Middle Name")]
     public string? MiddleName {get; set;}
     [Display(Name = "Last Name")]
-    public string? LastName { get; set; }
-    
+    public string LastName { get; set; }
+    [Display(Name = "Mailing Address")]
+    public string Address { get; set; }
+    [Display(Name = "Work Phone Number")]
+    public string WorkPhoneNumber { get; set; }
+    [Display(Name = "Personal Phone Number")]
+    public string PersonalPhoneNumber { get; set; }
+    [Display(Name = "Email Address")]
+    [DataType(DataType.EmailAddress)]
+    public string Emailaddress { get; set; }
+    [Display(Name = "Date of Birth")]
+    public DateTime DateOfBirth { get; set; }
+    [Display(Name = "Emergency Contact Full Name")]
+    public  string EmergencyContactFullName { get; set; }
+    [Display(Name = "Emergency Contact Phone Number")]
+    public  string EmergencyContactPhoneNumber { get; set; }
 }
